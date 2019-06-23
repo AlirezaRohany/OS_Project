a895050a8b051260195183b4f9a80947cfda55a9
Alireza Rohany <alireza.rohany.77@gmail.com>
2019-06-24 00:08:22 +0430

    Revert "Update proc.h"
    This reverts commit be8796296a276b6ad4dbd5ab44d3e7b833f1269a.

--- proc.h
+++ proc.h
@@ -49,10 +49,6 @@ struct proc {
   struct file *ofile[NOFILE];  // Open files
   struct inode *cwd;           // Current directory
   char name[16];               // Process name (debugging)
-  //newitems
-  int ctime;                 
-  int rtime;                  
-  int etime;                   
 };
 
 // Process memory is laid out contiguously, low addresses first:
